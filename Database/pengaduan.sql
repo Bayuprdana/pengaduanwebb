264245907be445b12b2461115e0a5e83d6cc46fd
121140032_Antonius Munthe <117933233+121140032@users.noreply.github.com>
2023-12-12 19:15:10 +0700

    Add files via upload

--- Database/pengaduan.sql
+++ Database/pengaduan.sql
@@ -1,1 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2023 at 09:08 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pengaduan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masyarakat`
+--
+
+CREATE TABLE `masyarakat` (
+  `nik` char(16) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `telp` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `masyarakat`
+--
+
+INSERT INTO `masyarakat` (`nik`, `nama`, `username`, `password`, `telp`) VALUES
+('12333', 'lo ga bahaya ta', 'bahaya', '1', '7078975'),
+('12345', 'Alip Arwan', 'aliparwan', '1', '08888888'),
+('12346', 'bayuaf', 'asfag', '1', '09776966'),
+('1995', 'booyah', 'boyy', '3', '012312414'),
+('333333', 'baatttt', 'bat', '2', '09932321'),
+('9999', 'broskyu', 'bro yak', '9', '0812345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaduan`
+--
+
+CREATE TABLE `pengaduan` (
+  `id_pengaduan` int(11) NOT NULL,
+  `kategori_pengaduan` enum('Pengaduan Fasilitas','Pengaduan Kebersihan','Pengaduan Kegiatan','Pengaduan Perangkat Desa','Pengaduan Keamanan') NOT NULL,
+  `tgl_pengaduan` date NOT NULL,
+  `nik` char(16) NOT NULL,
+  `isi_laporan` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `status` enum('0','DIPROSES','SELESAI') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengaduan`
+--
+
+INSERT INTO `pengaduan` (`id_pengaduan`, `kategori_pengaduan`, `tgl_pengaduan`, `nik`, `isi_laporan`, `foto`, `status`) VALUES
+(14, 'Pengaduan Fasilitas', '2023-12-10', '9999', 'coba 1', 'baru.png', ''),
+(15, '', '2023-12-11', '9999', 'kkkn', 'Database.png', 'SELESAI'),
+(16, '', '2023-12-11', '9999', 'asdasdasdasdaddasdasdf', 'Database.png', 'SELESAI'),
+(17, 'Pengaduan Kebersihan', '2023-12-11', '9999', 'kureg bersih', 'petugas.png', ''),
+(18, 'Pengaduan Kegiatan', '2023-12-12', '9999', 'yoyosssssssss', 'Dasboard(user).png', ''),
+(19, 'Pengaduan Keamanan', '2023-12-12', '9999', 'adasdsadasdasdasd', 'Daftarz.png', ''),
+(20, 'Pengaduan Fasilitas', '2023-12-05', '9999', 'asdasdasdsadasd', 'fitur Lihat pengaduan(user).png', ''),
+(21, 'Pengaduan Kebersihan', '2023-12-12', '9999', 'geblek', 'baru.png', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(35) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `level` enum('admin','petugas','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `telp`, `level`) VALUES
+(1, 'admin', 'admin', '1', '09987977', 'admin'),
+(2, 'petugas', 'petugas', '1', '0865456', 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tanggapan`
+--
+
+CREATE TABLE `tanggapan` (
+  `id_tanggapan` int(11) NOT NULL,
+  `kategori_pengaduan` varchar(255) DEFAULT NULL,
+  `tgl_pengaduan` date DEFAULT NULL,
+  `nik` varchar(255) DEFAULT NULL,
+  `isi_laporan` text DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `verifikasi` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `masyarakat`
+--
+ALTER TABLE `masyarakat`
+  ADD PRIMARY KEY (`nik`);
+
+--
+-- Indexes for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  ADD PRIMARY KEY (`id_pengaduan`);
+
+--
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  ADD PRIMARY KEY (`id_tanggapan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
