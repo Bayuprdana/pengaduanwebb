2010056a340e4cc7aa15e9286f22381358f568ed
121140032_Antonius Munthe <117933233+121140032@users.noreply.github.com>
2023-12-13 19:30:43 +0700

    Add files via upload

--- petugas/proses-verifikasi.php
+++ petugas/proses-verifikasi.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 
@@ -44,50 +43,3 @@ if (isset($_FILES['foto'])) {
     echo "<script>alert('$alert'); window.location.assign('petugas.php?url=verifikasi-laporan');</script>";
 }
 ?>
-=======
-<?php
-session_start();
-
-include '../koneksi/koneksi.php';
-
-$kategori_pengaduan = isset($_POST['kategori_pengaduan']) ? $_POST['kategori_pengaduan'] : '';
-$tgl_pengaduan = isset($_POST['tgl_pengaduan']) ? $_POST['tgl_pengaduan'] : '';
-$nik = isset($_POST['nik']) ? $_POST['nik'] : '';
-$isi_laporan = isset($_POST['isi_laporan']) ? $_POST['isi_laporan'] : '';
-$status = 0;
-
-
-$verifikasi = isset($_POST['verifikasi']) ? $_POST['verifikasi'] : array();
-
-if (isset($_FILES['foto'])) {
-    $lokasi_foto = $_FILES['foto']['tmp_name'];
-    $nama_foto = $_FILES['foto']['name'];
-
-    if (move_uploaded_file($lokasi_foto, '../foto/' . $nama_foto)) {
-
-        $sql = "INSERT INTO tanggapan(kategori_pengaduan, tgl_pengaduan, nik, isi_laporan, foto, status, verifikasi) 
-        VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-        $stmt = $koneksi->prepare($sql);
-        $stmt->bind_param("ssssssi", $kategori_pengaduan, $tgl_pengaduan, $nik, $isi_laporan, $nama_foto, $status, $verifikasi);
-        $stmt->execute();
-        $stmt->close();
-
-        foreach ($verifikasi as $verifikasiId) {
-            $verifikasiId = intval($verifikasiId);
-            if ($verifikasiId > 0) {
-                $updateSql = "UPDATE pengaduan SET status='SELESAI' WHERE id_pengaduan = $verifikasiId";
-                mysqli_query($koneksi, $updateSql);
-            }
-        }
-
-        echo 'Verifikasi berhasil dilakukan untuk pengaduan yang dipilih.';
-    } else {
-        echo 'Gagal mengunggah file foto.';
-    }
-} else {
-    $alert = 'Foto tidak diupload gagal diverifikasi.';
-    echo "<script>alert('$alert'); window.location.assign('petugas.php?url=verifikasi-laporan');</script>";
-}
-?>
->>>>>>> 264245907be445b12b2461115e0a5e83d6cc46fd
